d3daedd21b52cf621f7da831d78b5f9bd2279400
yogansh sharma <yogansh.sharma4000@gmail.com>
2026-06-15 18:05:08 +0530

    all dimension array

--- numpy_intro.py
+++ numpy_intro.py
@@ -1,5 +1,18 @@
 import numpy as np
 
-arr=np.array([1,2,3,4])
+arr0=np.array(112)
+arr1=np.array([1,2,3,4])
+arr2=np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+arr3=np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4],[1,2,3,4]]],ndmin=10)
+print(arr0)
+print(arr0.ndim)
 
-print(arr)
+print(arr1)
+print(arr1.ndim)
+
+
+print(arr2)
+print(arr2.ndim)
+
+print(arr3)
+print(arr3.ndim)

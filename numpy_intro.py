80ee3dea720df62af5cc9d87c00e3569e8cfb256
yogansh sharma <yogansh.sharma4000@gmail.com>
2026-06-18 13:26:25 +0530

    datatype conversions

--- numpy_intro.py
+++ numpy_intro.py
@@ -3,7 +3,7 @@ import numpy as np
 arr0=np.array(112)
 arr1=np.array([1,2,3,4])
 arr2=np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
-arr3=np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4],[1,2,3,4]]],ndmin=10)
+arr3=np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
 print(arr0)
 print(arr0.ndim)
 
@@ -16,3 +16,36 @@ print(arr2.ndim)
 
 print(arr3)
 print(arr3.ndim)
+
+
+# accessing the elements with indexing
+
+print(arr0)
+
+print(arr1[1])
+
+print(arr2[2,3])
+
+print(arr3[1,0,2])
+
+
+
+arrnew=np.array([1,2,3,4,5,6,7,8])
+print(arrnew[1:5:])
+
+
+arrnext=np.array([1,2,3,4],dtype='i4')
+print(arrnext)
+print(arrnext.dtype)
+
+
+
+arr9=np.array([1.5,5,2,5,6,3])
+arrdub=arr9.astype('i8')
+print(arr9.dtype)
+print(arrdub.dtype)
+
+arr10=np.array([1,0,3,0,4])
+arr10dub=arr10.astype(bool)
+print(arr10.dtype)
+print(arr10dub.dtype)
